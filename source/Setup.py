8125bd985f4f76f87a2c1afc24f0b370d1a551bd
yaseenali99 <yaseenali99@gmail.com>
2019-12-11 22:04:51 -0500

    Cleaned and Commented Descriptions Module. Setup with code for handling multiple routines in the Input file. Produces a line in the galaxy load file per routine

--- source/Setup.py
+++ source/Setup.py
@@ -10,29 +10,38 @@ plc = L5k.L5kObject(fname)
 #Read in Excel File
 data = pd.read_excel("..\data\Input_phase_list.xlsx",sheet_name="Sheet1")
 programs = data["Program"].unique()
-routines = data["Routine"].unique()
+routines = data["Routine"].unique() #List of routines in the input phase list
 
 
-#Read in Galaxy load file
+#Read in Galaxy load  template
 df_galaxy = pd.read_csv("..\data\\test_phase.csv")
-desc_list = (df_galaxy.iloc[1,1]).split(',') #Getting current step descriptions (list of len 100)
-desc_list.insert(0,'Step Description 0')
+template_desc_list = (df_galaxy.iloc[1,1]).split(',') #Getting current template step descriptions (list of len 100)
+template_desc_list.insert(0,'Step Description 0') #Adding in a dummy Step 0 description so the list lines up with Archestra's array starting at 1
+#Later we will drop the first 0 step desc
 #print("Desc list: ",desc_list)
-object_name = df_galaxy.iloc[0,1]
 
+#Grabs tagname for no good reason since this is the template
+object_name = df_galaxy.iloc[0,1]
+df_temp = df_galaxy.iloc[:1,:].copy()
 
 for routine in routines:
         routine_text = plc['CONTROLLER']['PROGRAM '+ programs[0]]['ROUTINE ' + routine]['text']
-        stp = ds.routine(routine_text) #Instantiating routine class
-        step_dict = stp.build_step_dict() #building step dict
+        rtne = ds.routine(routine_text) #Instantiating routine class
+        #print(rtne.name)
+        step_dict = rtne.build_step_dict() #building step dict. To debug pass in debug=1 as a parameter
+        desc_list = template_desc_list.copy() #Create a copy of the template to make changes to
         for key in step_dict:
-                desc_list[int(key)] = step_dict[key] #Update description list with description from dict
+                desc_list[int(key)] = step_dict[key] #Update the new description list with descriptions from the dict
 
-        #prepare line to be appended to
-        df_galaxy.iloc[1,1] = (',').join(desc_list[1:])
-        #print(df_galaxy)
-        df_galaxy.to_csv("..\data\\test_phase_mod.csv",index=None)#,quoting=csv.QUOTE_NONE,quotechar='',  escapechar="\\")
+        #Append Line
+
+        df_temp_line = pd.DataFrame([[rtne.name,(',').join(desc_list[1:])]],columns=df_galaxy.columns)
+        #print(df_temp_line)
+        df_temp = df_temp.append(df_temp_line,ignore_index=True)
+
+df_temp.to_csv("..\data\\test_phase_mod.csv",index=None)#,quoting=csv.QUOTE_NONE,quotechar='',  escapechar="\\")
         #print(stp.get_phase_name(), step_dict.items())
+print(df_temp)
 
 print("")
 
